e492a5ff3106afec8a7647045842b93b5fd20d78
Shameekh Naveed <shameekh2002@gmail.com>
2024-02-24 22:05:05 +0500

    n

--- components/CarouselHome.tsx
+++ components/CarouselHome.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+"use client"
+import React, { useEffect, useState } from "react"
 import {
 	Carousel,
 	CarouselContent,
@@ -7,14 +8,26 @@ import {
 	CarouselNext
 } from "./ui/carousel"
 import Card from "./CarouselCard"
+import { toast } from "sonner"
+import { Tour } from "@/app/database/schemas/tour.schema"
 
-const CarouselHome = () => {
+export default function CarouselHome() {
+	const [data, setData] = useState([])
+	useEffect(() => {
+		fetchData().then(data => setData(data))
+	}, [])
+
 	return (
 		<Carousel className="container">
 			<CarouselContent>
-				{Array.from({ length: 5 }).map((_, index) => (
+				{data.map((tour: Tour, index: number) => (
 					<CarouselItem className="sm:basis-1/2 min-[1300px]:basis-1/4 min-[1495px]:basis-1/3">
-						<Card />
+						<Card
+							name={tour.title as string}
+							description={tour.description as string}
+							image={tour.image as string}
+							link={`/tour/${tour._id as string}`}
+						/>
 					</CarouselItem>
 				))}
 			</CarouselContent>
@@ -24,4 +37,21 @@ const CarouselHome = () => {
 	)
 }
 
-export default CarouselHome
+const fetchData = async () => {
+	try {
+		const res = await fetch(
+			process.env.HOST || "http://localhost:3000/" + `api/tour/trending`
+		)
+		const parsedRes = await res.json()
+		if (!res.ok) {
+			toast.error(parsedRes.message)
+			toast.error(parsedRes.error)
+		}
+		const tours = parsedRes.data.tours
+		return tours
+	} catch (err) {
+		return []
+	}
+}
+
+// export default CarouselHome
