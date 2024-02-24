c6d51aa26b4e317696971de2cd357be14fbbc1b4
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2024-02-24 12:24:57 +0500

    feat: Adding preferences

---
+++ app/[lang]/preferences/page.tsx
@@ -0,0 +1,18 @@
+const Preferences = () => {
+	return (
+		<main className="flex min-h-[calc(100vh-192px)] justify-end bg-black">
+			<div className="my-auto text-center px-8">
+				<h1 className="text-white text-5xl font-semibold">
+					Let's start your travel journey
+				</h1>
+			</div>
+			<div className="lg:w-[70%] bg-white rounded-tl-3xl flex justify-center items-center">
+				<h1 className="text-xl">
+					Select your preferences for your journey
+				</h1>
+			</div>
+		</main>
+	)
+}
+
+export default Preferences
