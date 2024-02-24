0a70895ca5ead062730bc91dd8eaddf64d1b530e
Shameekh Naveed <shameekh2002@gmail.com>
2024-02-24 12:19:18 +0500

    chore: tour schema modifi

--- app/database/schemas/tour.schema.ts
+++ app/database/schemas/tour.schema.ts
@@ -3,6 +3,20 @@ import { UserRole, UserStatus } from "@/app/enums/user.enum"
 import { Schema, InferSchemaType, model } from "mongoose"
 import { types } from "util"
 
+const attractionSchema = new Schema({
+	time: { type: String, required: true },
+	title: { type: String, required: true },
+	description: { type: String }
+})
+
+const itinerarySchema = new Schema({
+	title: { type: String, required: true },
+	description: { type: String },
+	day: { type: Number, required: true },
+	nightStay: { type: String },
+	attraction: [{ type: attractionSchema, default: [] }]
+})
+
 const tourSchema = new Schema(
 	{
 		title: { type: String, required: true },
@@ -14,6 +28,7 @@ const tourSchema = new Schema(
 			ref: "User",
 			required: true
 		},
+		itinerary: [{ type: itinerarySchema, default: [] }],
 		totalAmount: {
 			type: Number,
 			required: true
@@ -33,7 +48,12 @@ const tourSchema = new Schema(
 )
 
 const TourModel = model("Tour", tourSchema)
-type Tour = InferSchemaType<typeof tourSchema>
+const ItineraryModel = model("Itinerary", itinerarySchema)
+const AttractionModel = model("Attraction", attractionSchema)
 
-export { TourModel }
-export type { Tour }
+type Tour = InferSchemaType<typeof tourSchema>
+type Itinerary = InferSchemaType<typeof itinerarySchema>
+type Attraction = InferSchemaType<typeof attractionSchema>
+
+export { TourModel, ItineraryModel, AttractionModel }
+export type { Tour, Itinerary, Attraction }
