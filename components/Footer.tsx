f17bc13b31adf407bb7c837d03c6850d7bab11c7
Taha <tahamukhtar20@gmail.com>
2024-02-24 12:02:23 +0500

    feat: add layout, .vscode, prettier, lang sup

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="bg-secondary w-full flex">
+			<div className="container flex justify-center items-center h-24 mx-auto"></div>
+		</footer>
+	)
+}
