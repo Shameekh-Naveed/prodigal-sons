67399d9eb520d2b568d88c4528731180b5267562
Shameekh Naveed <shameekh2002@gmail.com>
2024-02-24 23:41:06 +0500

    mm

--- utils/query-parser.ts
+++ utils/query-parser.ts
@@ -1,4 +1,4 @@
-function paginationParser(page: number, inputLimit: number) {
+export function paginationParser(page: number, inputLimit: number) {
 	const limit = inputLimit > 0 ? inputLimit : 10
 	const skip = page * limit
 	return [limit, skip]
