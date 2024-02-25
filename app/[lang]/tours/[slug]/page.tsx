d91f65bde0a97b0509833135bc94ac14ea1e27b2
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2024-02-25 06:01:36 +0500

    fix: slug page

--- app/[lang]/tours/[slug]/page.tsx
+++ app/[lang]/tours/[slug]/page.tsx
@@ -1,11 +1,17 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import { Locale } from "@/i18n.config"
 import { getDictionary } from "@/lib/dictionaries"
+import Image from "next/image"
+import Kaaba from "@/assets/kaaba.png"
+import { FaCalendar } from "react-icons/fa"
+import { IoMdMoon } from "react-icons/io"
+import { MdOutlineFlightTakeoff } from "react-icons/md"
+import { IoFastFood } from "react-icons/io5"
 
-export default async function Page({ params }: { params: { lang: Locale } }) {
-	const { tripSlug } = await getDictionary(params.lang)
+export default async function Page() {
 	const trip = {
-		title: "something",
+		title: "Shahi Masjid",
 		description:
 			"something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something ",
 		departure: new Date().toISOString().slice(0, 10),
@@ -42,8 +48,8 @@ export default async function Page({ params }: { params: { lang: Locale } }) {
 			}
 		],
 		totalAmount: 123456,
-		status: "pending",
-		type: "religious"
+		status: "Pending",
+		type: "Religious Trip"
 	}
 	return (
 		<main
@@ -53,22 +59,72 @@ export default async function Page({ params }: { params: { lang: Locale } }) {
 			}}
 		>
 			<div className="container mx-auto">
-				<div className="w-full min-h-96 mt-28 bg-secondary rounded-t-xl p-5 flex flex-col gap-2">
-					<h1 className="font-bold text-3xl">{tripSlug.yourTrip}</h1>
-					<div className="border w-full border-primary max-w-2xl" />
+				<div className="w-full min-h-96 mt-28 bg-secondary rounded-t-xl p-5 pl-10 flex flex-col gap-2">
+					{/* <div className="border w-full border-primary max-w-2xl" /> */}
 					<div className="w-full">
-						<h2 className="font-semibold text-2xl">{trip.title}</h2>
-						<div className="flex flex-row gap-2 font-light">
-							<h2 className="">
-								{tripSlug.type}: {trip.type}
-							</h2>
-							<h2 className="">
-								{tripSlug.status}: {trip.status}
-							</h2>
+						<h2 className="font-bold text-5xl my-4">
+							{trip.title}
+						</h2>
+						<div className="flex flex-col min-[1200px]:flex-row max-[1200px]:items-start ">
+							<div className="relative w-full h-[20rem] md:h-[30rem]">
+								<Image
+									src={Kaaba}
+									alt="Ka'aba"
+									fill
+									className="border-4 border-primary rounded-xl object-contain"
+								/>
+							</div>
+							<div className="flex flex-col text-xl font-light gap-4 ml-4 mt-8 min-[1200px]:ml-24">
+								<div className=" font-semibold">
+									Type: {trip.type}
+								</div>
+								<div className="font-semibold">
+									Status: {trip.status}
+								</div>
+								<div className="text-5xl font-semibold">
+									$ {trip.totalAmount} / person
+								</div>
+								<div className="grid grid-cols-2 gap-8 mt-4">
+									<div className="flex">
+										<FaCalendar className="mt-1" />
+										<h3 className="ml-2 text-md">
+											{trip.departure}- {trip.arrival}
+										</h3>
+									</div>
+									<div className="flex">
+										<IoMdMoon className="mt-1" />
+										<h3 className="ml-2 text-md">
+											{trip.itinerary.length} nights
+										</h3>
+									</div>
+									<div className="flex">
+										<MdOutlineFlightTakeoff className="mt-1" />
+										<h3 className="ml-2 text-md">
+											comfortable flight
+										</h3>
+									</div>
+
+									<div className="flex">
+										<IoFastFood className="mt-1" />
+										<h3 className="ml-2 text-md">
+											food included
+										</h3>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-					<desc className="text-lg ">{trip.description}</desc>
-					<h1></h1>
+					<h1 className="text-2xl font-semibold mt-8">
+						Trip discription
+					</h1>
+					<desc className="text-xl ">{trip.description}</desc>
+					<h1 className="text-xl font-semibold mt-4">
+						Trip Organizer : {trip.organizerID}
+					</h1>
+
+					<h1 className="text-2xl font-semibold mt-8">
+						Trip Itinerary
+					</h1>
 					{trip.itinerary.map((itinerary: any, index: number) => (
 						<ol className="relative border-s border-gray-200 dark:border-gray-700">
 							<Itinerary itinerary={itinerary} key={index} />
@@ -79,7 +135,7 @@ export default async function Page({ params }: { params: { lang: Locale } }) {
 							Rs. {trip.totalAmount}
 						</h2>
 						<div>
-							<Button>{tripSlug.checkout}</Button>
+							<Button>Checkout</Button>
 						</div>
 					</div>
 				</div>
